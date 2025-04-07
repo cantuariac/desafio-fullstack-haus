6f3357d381712e2828d428df7abc600b56aaf6ef
cantuariac <caio.cantuaria@gmail.com>
2025-04-07 11:03:26 -0300

    Projeto inicial com acesso ao banco e Identity

---
+++ DesafioFullStackHaus.Server/Data/DesafioHausDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioFullStackHaus.Server.Data
+{
+
+    public class DesafioHausDbContext : IdentityDbContext<IdentityUser>
+    {
+        public DesafioHausDbContext(DbContextOptions<DesafioHausDbContext> options) : base(options) { }
+    }
+}
---
+++ DesafioFullStackHaus.Server/Program.cs
@@ -0,0 +1,41 @@
+using DesafioFullStackHaus.Server.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<DesafioHausDbContext>(options => options.UseNpgsql(connectionString: builder.Configuration.GetConnectionString("PostgresConnection")));
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<DesafioHausDbContext>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
